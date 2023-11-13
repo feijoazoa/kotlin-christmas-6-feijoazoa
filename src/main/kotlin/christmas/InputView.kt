e7f9138ac8ca52fc27f1718ce13cb1333ad6878a
eunyeong <feijoaplz@gmail.com>
2023-11-13 11:33:46 +0900

    feat: 유효한 날짜를 받는 기능

--- src/main/kotlin/christmas/InputView.kt
+++ src/main/kotlin/christmas/InputView.kt
@@ -1,4 +1,33 @@
 package christmas
 
+import camp.nextstep.edu.missionutils.Console
+
 class InputView {
+    fun readDate(): Int {
+        println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.")
+        println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)")
+        while (true) {
+            val inputDate = Console.readLine() ?: ""
+            try {
+                validateDateInput(inputDate)
+                return inputDate.toInt()
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
+    }
+
+    private fun validateDateInput(inputDate: String) {
+        require(inputDate.isNotBlank()) { EMPTY_INPUT_ERROR }
+        require(inputDate.toIntOrNull() != null) { NON_NUMERIC_INPUT_ERROR }
+        val date = inputDate.toInt()
+        require(date in 1..31) { INVALID_DATE_INPUT_ERROR }
+    }
+
+    companion object {
+        private const val EMPTY_INPUT_ERROR = "[ERROR] 값을 입력해 주세요."
+        private const val NON_NUMERIC_INPUT_ERROR = "[ERROR] 숫자만 입력해 주세요."
+        private const val INVALID_DATE_INPUT_ERROR = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."
+    }
 }
+
