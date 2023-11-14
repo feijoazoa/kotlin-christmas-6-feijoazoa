bb6d983a0e3d63361954659f482d7864d4a30ffc
eunyeong <feijoaplz@gmail.com>
2023-11-15 02:04:49 +0900

    refactor: 매직넘버 상수화

--- src/main/kotlin/christmas/Discount.kt
+++ src/main/kotlin/christmas/Discount.kt
@@ -2,14 +2,21 @@ package christmas
 
 class Discount(private val order: Order) {
     fun christmasDdayDiscount(dayOfMonth: Int): Int {
-        return 1000 + (dayOfMonth - 1) * 100
+        return BASE_CHRISTMAS_DISCOUNT + (dayOfMonth - 1) * CHRISTMAS_DISCOUNT_INCREMENT
     }
     fun weekdayDiscount(): Int {
-        return order.getDessertCount() * 2023
+        return order.getDessertCount() * WEEKDAY_DISCOUNT_DISCOUNT_PRICE
     }
     fun weekendDiscount(): Int {
-        return order.getMainCount() * 2023
+        return order.getMainCount() * WEEKEND_DISCOUNT_DISCOUNT_PRICE
     }
+    companion object {
+        const val BASE_CHRISTMAS_DISCOUNT = 1000
+        const val CHRISTMAS_DISCOUNT_INCREMENT = 100
+        const val WEEKDAY_DISCOUNT_DISCOUNT_PRICE = 2023
+        const val WEEKEND_DISCOUNT_DISCOUNT_PRICE = 2023
+    }
+
 }
 data class EventResult(
     val benefits: List<String>,
--- src/main/kotlin/christmas/Event.kt
+++ src/main/kotlin/christmas/Event.kt
@@ -10,63 +10,75 @@ class Event(private val order: Order, private val date: LocalDate) {
     private val gifts = mutableListOf<String>()
 
     fun applyEvents(): EventResult {
-        if (order.getTotalAmount() < 10000) {
+        if (order.getTotalAmount() < MINIMUM_TOTAL_ORDER_PRICE) {
             return EventResult(emptyList(), emptyList(), 0, order.getTotalAmount())
         }
         var totalDiscount = 0
-        var giftEventAmount = 0
+        var giftEventPrice = 0
 
         val dayOfWeek = date.dayOfWeek.name
         val dayOfMonth = date.dayOfMonth
 
-        totalDiscount += applyChristmasEvent(dayOfMonth)
+        totalDiscount += applyChristmasDdayEvent(dayOfMonth)
         totalDiscount += applyWeekdayEvent(dayOfWeek)
         totalDiscount += applySpecialDayEvent(dayOfMonth)
-        giftEventAmount = applyGiftEvent()
+        giftEventPrice = applyGiftEvent()
 
         val totalAmountAfterDiscount = order.getTotalAmount() - totalDiscount
 
-        return EventResult(benefits, gifts, totalDiscount + giftEventAmount, totalAmountAfterDiscount)
+        return EventResult(benefits, gifts, totalDiscount + giftEventPrice, totalAmountAfterDiscount)
     }
-    private fun applyChristmasEvent(dayOfMonth: Int): Int {
-        return if (date.monthValue == 12 && dayOfMonth in 1..25) {
-            val discountAmount = discount.christmasDdayDiscount(dayOfMonth)
-            benefits.add("크리스마스 디데이 할인: -${discountAmount.format()}원")
-            discountAmount
+    private fun applyChristmasDdayEvent(dayOfMonth: Int): Int {
+        return if (date.monthValue == CHRISTMAS_MONTH && dayOfMonth in CHRISTMAS_DAYS) {
+            val discountPrice = discount.christmasDdayDiscount(dayOfMonth)
+            benefits.add("크리스마스 디데이 할인: -${discountPrice.format()}원")
+            discountPrice
         } else 0
     }
     private fun applyWeekdayEvent(dayOfWeek: String): Int {
         return when (dayOfWeek) {
-            in listOf("SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY") -> {
-                val discountAmount = discount.weekdayDiscount()
-                benefits.add("평일 할인: -${discountAmount.format()}원")
-                discountAmount
+            in WEEKDAYS -> {
+                val weekdaysDiscountPrice = discount.weekdayDiscount()
+                benefits.add("평일 할인: -${weekdaysDiscountPrice.format()}원")
+                weekdaysDiscountPrice
             }
-            in listOf("FRIDAY", "SATURDAY") -> {
-                val discountAmount = discount.weekendDiscount()
-                benefits.add("주말 할인: -${discountAmount.format()}원")
-                discountAmount
+            in WEEKENDS -> {
+                val weekendsDiscountPrice = discount.weekendDiscount()
+                benefits.add("주말 할인: -${weekendsDiscountPrice.format()}원")
+                weekendsDiscountPrice
             }
             else -> 0
         }
     }
     private fun applySpecialDayEvent(dayOfMonth: Int): Int {
-        return if (dayOfMonth in listOf(3, 10, 17, 24, 25, 31)) {
-            val discountAmount = 1000
-            benefits.add("특별 할인: -${discountAmount.format()}원")
-            discountAmount
+        return if (dayOfMonth in SPECIAL_DAYS) {
+            val specialDayDiscountPrice = SPECIAL_DAY_DISCOUNT_PRICE
+            benefits.add("특별 할인: -${specialDayDiscountPrice.format()}원")
+            specialDayDiscountPrice
         } else 0
     }
     private fun applyGiftEvent(): Int {
-        return if (order.getTotalAmount() >= 120000) {
+        return if (order.getTotalAmount() >= GIFT_EVENT_AMOUNT_THRESHOLD) {
             gifts.add("샴페인 1개")
-            val giftEventAmount = 25000
-            benefits.add("증정이벤트: -${giftEventAmount.format()}원")
-            giftEventAmount
+            val giftEventPrice = GIFT_EVENT_PRICE
+            benefits.add("증정이벤트: -${giftEventPrice.format()}원")
+            giftEventPrice
         } else 0
     }
 
     private fun Int.format(): String {
         return NumberFormat.getNumberInstance(Locale.KOREA).format(this)
+    }
+
+    companion object {
+        const val MINIMUM_TOTAL_ORDER_PRICE = 10000
+        const val CHRISTMAS_MONTH = 12
+        const val GIFT_EVENT_AMOUNT_THRESHOLD = 120000
+        const val SPECIAL_DAY_DISCOUNT_PRICE = 1000
+        const val GIFT_EVENT_PRICE = 25000
+        val CHRISTMAS_DAYS = 1..25
+        val SPECIAL_DAYS = listOf(3, 10, 17, 24, 25, 31)
+        val WEEKDAYS = listOf("SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY")
+        val WEEKENDS = listOf("FRIDAY", "SATURDAY")
     }
 }
