7ae2908edd7b76b0c2c9b5891448af6d93043a56
eunyeong <feijoaplz@gmail.com>
2023-11-14 20:22:45 +0900

    feat: 각 이벤트에 맞는 할인을 계산하는 기능

--- src/main/kotlin/christmas/Discount.kt
+++ src/main/kotlin/christmas/Discount.kt
@@ -5,14 +5,12 @@ class Discount(private val order: Order) {
         return 1000 + (dayOfMonth - 1) * 100
     }
     fun weekdayDiscount(): Int {
-        return order.getMenuNames(MenuCategory.DESSERT) * 2023
+        return order.getDessertCount() * 2023
     }
     fun weekendDiscount(): Int {
-        return order.getMenuNames(MenuCategory.MAIN) * 2023
+        return order.getMainCount() * 2023
     }
-
 }
-
 data class EventResult(
     val benefits: List<String>,
     val gifts: List<String>,
