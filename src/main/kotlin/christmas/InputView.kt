a9edfaef24ff38eba738112214230e5d2bc64f6b
eunyeong <feijoaplz@gmail.com>
2023-11-15 02:17:33 +0900

    Refactor: 매직넘버 상수화

--- src/main/kotlin/christmas/InputView.kt
+++ src/main/kotlin/christmas/InputView.kt
@@ -4,8 +4,8 @@ import camp.nextstep.edu.missionutils.Console
 
 class InputView {
     fun readDate(): Int {
-        println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.")
-        println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)")
+        println(WELCOME_MESSAGE)
+        println(REQUEST_DATE_INPUT_MESSAGE)
         while (true) {
             val inputDate = Console.readLine() ?: ""
             try {
@@ -19,17 +19,17 @@ class InputView {
 
     fun getOrder(menu: Menu): String {
         while (true) {
-            println("주문할 메뉴와 개수를 입력해주세요 (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)")
+            println(REQUEST_MENU_ORDER_MESSAGE)
             val order = Console.readLine()?.trim()
             if (order != null && menu.isValidMenuOrder(order)) {
                 return order
             } else {
-                println("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.")
+                println(INVALID_ORDER_INPUT_ERROR)
             }
         }
     }
     private fun validateDateInput(inputDate: String) {
-        require(inputDate.isNotBlank()) { EMPTY_INPUT_ERROR }
+        require(inputDate.isNotBlank()) { INVALID_DATE_INPUT_ERROR }
         require(inputDate.toIntOrNull() != null) { INVALID_DATE_INPUT_ERROR }
         val validDate = inputDate.toInt()
         require(validDate in 1..31) { INVALID_DATE_INPUT_ERROR }
@@ -38,8 +38,11 @@ class InputView {
 
     companion object
     {
-        private const val EMPTY_INPUT_ERROR = "[ERROR] 값을 입력해 주세요."
+        private const val WELCOME_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."
+        private const val REQUEST_DATE_INPUT_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"
+        private const val REQUEST_MENU_ORDER_MESSAGE = "주문할 메뉴와 개수를 입력해주세요 (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"
         private const val INVALID_DATE_INPUT_ERROR = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."
+        private const val INVALID_ORDER_INPUT_ERROR = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."
     }
 }
 
