ff3d52ff9e41452cbdf1731d536b2c2a0eac524c
eunyeong <feijoaplz@gmail.com>
2023-11-15 14:53:14 +0900

    Test: Order 클래스에 대한 Test 코드 작성

--- src/main/kotlin/christmas/InputView.kt
+++ src/main/kotlin/christmas/InputView.kt
@@ -40,13 +40,12 @@ class InputView {
     }
 
 
-    companion object
-    {
+    companion object {
         private const val WELCOME_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."
         private const val REQUEST_DATE_INPUT_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"
-        private const val REQUEST_MENU_ORDER_MESSAGE = "주문할 메뉴와 개수를 입력해주세요 (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"
-        private const val INVALID_DATE_INPUT_ERROR = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."
-        private const val INVALID_ORDER_INPUT_ERROR = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."
+        private const val REQUEST_MENU_ORDER_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"
+        internal const val INVALID_DATE_INPUT_ERROR = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."
+        internal const val INVALID_ORDER_INPUT_ERROR = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."
         private val VALID_DATE_RANGE = 1..31
     }
 }
